ca7364b4e2b134b0f316badb79542b1a86abf5ff
HeyitzSagar <sagargupta.gola@gmail.com>
2024-02-14 13:10:09 +0530

    using env connected mongodb

---
+++ api/config/db.js
@@ -0,0 +1,23 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
-
+const connectDb = require("./config/db");
+const { run } = require("node:test");
 const app = express();
+run();
 
 app.get("/test", (req, res) => {
   res.json("test ok");
 });
 
-app.post('/register', (req, res) => {
-    
-})
+app.post("/register", (req, res) => {});
 app.listen(4000, () => {
   console.log("App is running");
 });
