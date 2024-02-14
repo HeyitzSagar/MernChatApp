88a8af5354c3f23f511f478c62c371b76be7d4ad
HeyitzSagar <sagargupta.gola@gmail.com>
2024-02-14 18:01:15 +0530

    Post, JWT, Endpoint, axios done

--- api/index.js
+++ api/index.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const connectDb = require("./config/db");
 const { run } = require("node:test");
+const PORT = 4000;
 const app = express();
+const User = require("./models/UserModels.js");
+const jwt = require("jsonwebtoken");
 run();
 
+const jwtSecret = process.env.JWT_SECRET;
 app.get("/test", (req, res) => {
   res.json("test ok");
 });
 
-app.post("/register", (req, res) => {});
-app.listen(4000, () => {
-  console.log("App is running");
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const UniqueUser = await User.create(username, password);
+  jwt.sign({ userId: UniqueUser._id }, jwtSecret, (err, token) => {
+    if (err) {
+      throw err;
+    }
+    res.cookie("token", token).status(201).json("ok");
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App is running on the ${PORT}`);
 });
